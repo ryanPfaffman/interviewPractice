de05cf623411ee9d3dd8fdb8c5cfc7ea778c7387
ryanPfaffman <73614186+ryanPfaffman@users.noreply.github.com>
2022-11-24 06:57:04 -0600

    Add files via upload

---
+++ isHappy.py
@@ -0,0 +1,30 @@
+def isHappy(n):
+    def getNext(num):
+        rtn = 0
+        for x in str(num):
+            rtn += int(x)**2
+            print(rtn)
+        return rtn
+    hashm = {}
+
+    nxt = getNext(n)
+    print(f'nxt: {nxt}')
+    if nxt == 1:
+        return True
+    elif nxt == n:
+        return False
+
+    i = 0
+    hashm[i] = nxt
+
+    while True:
+        nxt = getNext(nxt)
+        print(f'nxt: {nxt}')
+        i += 1
+        if nxt in hashm.values():
+            return False
+        elif nxt == 1:
+            return True
+        else:
+            hashm[i] = nxt
+print(isHappy(102304928304923))
